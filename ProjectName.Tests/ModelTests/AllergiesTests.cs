786b30d622ff3c08dc4d6bb3a6e481f9851d26f6
Liam Cammpbell <liam22campbell@gmail.com>
2023-01-24 14:20:42 -0800

    Failed chocolate test

--- ProjectName.Tests/ModelTests/AllergiesTests.cs
+++ ProjectName.Tests/ModelTests/AllergiesTests.cs
@@ -45,35 +45,20 @@ namespace Allergies.Tests
     
     CollectionAssert.AreEqual(expected: newList, actual: result);
   }
-//     [TestMethod]
-//     public void ItemConstructor_CreatesInstanceOfItem_Item()
-//     {
-//       Item newItem = new Item();
-//       Assert.AreEqual(typeof(Item), newItem.GetType());
-//     }
+    [TestMethod]
+   public void CheckForChocolate_List()
+  {
+    //Arrange
+    Allergens allergies = new Allergens();
+    List<string> allergiesList = new List<string>();
+    List<string> newList = new List<string>();
+    newList.Add(item: "chocolate");
+    
+    List<string> result = allergies.AllergicTo(Score: 32,  allergiesList: allergiesList);
+    
+    CollectionAssert.AreEqual(expected: newList, actual: result);
+  }
 
-//     [TestMethod]
-//     public void GetDescription_ReturnsDescription_String()
-//     {
-//       string description = "Walk the dog.";
-//       Item newItem = new Item(description);
-//       string result = newItem.Description;
-//       Assert.AreEqual(description, result);
-//     }
-
-//     [TestMethod]
-// public void GetDescription_ReturnsDescription_String()
-// {
-//   //Arrange
-//   string description = "Walk the dog.";
-//   Item newItem = new Item(description);
-
-//   //Act
-//   string result = newItem.Description;
-
-//   //Assert
-//   Assert.AreEqual(description, result);
-// }
 
   }
 }
